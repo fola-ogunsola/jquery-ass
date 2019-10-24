406969972adc7174272e5d4ad9cc0ffbb9bb8584
fola-ogunsola <54547114+fola-ogunsola@users.noreply.github.com>
2019-10-24 08:38:34 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+$(document).ready(function (){
+	$(".para").click(function () {
+		$(".para").css("background-color", "yellow")
+	})
+})
+//number 2
+// $(document).ready(function () {
+//  $("form input").hide()
+// });
+
+//number 3
+// $(document).ready(function () {
+//     $('<div><h1>hello world</h1></div>').appendTo('body');
+// });
+
+//number 4
+// $(document).ready(function () {
+// 	$(".hey")(function (){
+
+// 	
+
+
+//number 6
+// $(document).ready(function () {
+// 	$("button").click(function () {
+// 		$(".para").css({"background-color": "grey", "color": "pink", "font-size": "100%"})
+// 	});
+// });
+
+//number 7
+
+// $(document).ready(function(){
+// 	$("p").dblclick(function (){
+// 		$("p").toggle(1500)
+// 	});
+// });
+
+//number 10
+
+// $(document).ready(function () {
+// 	$(".para").click(function (){
+// 		$(this).toggle(1500)
+// 	});
+// });
+
+//number 9
+// $(document).ready(function (){
+// 	$("#id_input_text16").val("Enter data");
+// });
+
+// $(document).ready(function (){
+// 	$("p").first().css("background", "green");
+// });
+
+// $(document).ready(function (){
+// 	$("li").not(".first, .last").css({"color":"red", "background":"yellow"});
+// });
+
+// $(document).ready(function (){
+// 	$("p").has("span").css({"background":"green", "font-size":"12px","height":"50px", "width":"100px"});
+// 
+
+$(document).ready(function(){
+ $("li").siblings(".bold").css("background-color", "lightgreen");
+});
